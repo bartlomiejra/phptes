602df70268ed88cbe1d8b242fd62668897beab78
bartlomiejra <bartlomiej.ra@gmail.com>
2021-07-07 18:30:53 +0200

    ASD

--- mail.php
+++ mail.php
@@ -10,26 +10,27 @@ if (!isset($_POST['email']))
 
 }
 if(isset($_POST['send_message_btn'])){
-	$to = "administrator@gmail.com";
 	$subject = $_POST['subject'];
 	$body = $_POST['body'];
 	$usermail = $_POST['usermail'];
 	
 	$headers = "Od: '${usermail}' email";
 
-	$to = "administrator@gmail.com";
+	$to = "elcebula@gmail.com";
 	$subject;
 	$body;
-	$headers = "Od: '${usermail}' email";
+	$headers = "Od: '${usermail}' ";
 	 
 	if( mail($to, $body, $subject,  $headers)) {
 // echo "Email  sent to ...";
 naglowek('<p class="mailOk">' ."Mail Wysłany 📧 " . '</p>');
-echo  "tu jest body: " . $body . "do kogo: " . $to . "temat: " . "$subject" . "Od Kogo: " . $headers;
-header( "refresh:10;url=index.php" );
+// echo  "tu jest body: " . $body . "do kogo: " . $to . "temat: " . "$subject" . "Od Kogo: " . $headers;
+header( "refresh:5;url=index.php" );
 
 	} else {
 		naglowek('<p class="mailError">' ."Błąd przy wysyłaniu wiadomości " . '</p>');
+		header( "refresh:5;url=index.php" );
+
 
 
 	}
